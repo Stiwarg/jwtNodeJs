ee95be792451328a9ee2b0b5b99a81b620d222f7
Jhon Estiven <jhonestiveng44@gmail.com>
2024-09-06 17:45:35 -0500

    Autentificación JWT

---
+++ src/config/config.js
@@ -0,0 +1,5 @@
+export const  {
+    PORT = 3000,
+    SALT_ROUNDS = 10,
+    SECRET_JWT_KEY =  'esto-es-una-prueba-para-el-mejor-programador-del-fucking-world-3$/gfd65545'
+} = process.env;
---
+++ src/database/user-repository.js
@@ -0,0 +1,66 @@
+import DBLocal from 'db-local';
+import crypto from 'crypto';
+import bcrypt from 'bcrypt';
+import { SALT_ROUNDS } from '../config/config.js';
+
+const { Schema } = new DBLocal({ path: './data' });
+
+const User = Schema('User', {
+    _id: { type: String, required: true },
+    username: { type: String, required: true },
+    password: { type: String, required: true }
+});
+
+export class UserRepository {
+    static async create ({ username, password }) {
+
+        Validation.username( username );
+        Validation.password( password );
+
+
+        const user = User.findOne({ username });
+        
+        if ( user ) throw new Error('Username already exists');
+        const id = crypto.randomUUID();
+        const hashedPassword = await bcrypt.hash( password, SALT_ROUNDS)
+        const hashedPassword2 = bcrypt.hashSync( password, SALT_ROUNDS)
+
+
+        User.create({
+            _id: id,
+            username,
+            password: hashedPassword 
+        }).save(); 
+            
+        return id;
+    }
+
+    static async  login ({ username, password }) {
+
+        Validation.username( username );
+        Validation.password( password );
+
+        const user = User.findOne({ username });
+        if ( !user ) throw new Error('Username does not exist');
+
+        const isValid = await bcrypt.compare( password, user.password );
+        if ( !isValid ) throw new Error('Password is invalid');
+        
+        const { password: _, ...publicUser } = user;
+
+        return publicUser;
+    
+    }
+}
+
+class Validation {
+    // Se puede utilizar tambien Zod para no realizar estas cosas
+    static username ( username ) {
+        if ( typeof username !== 'string' ) throw new Error('Username must be a string');
+        if ( username.length < 3 ) throw new Error('Username must be at least 3 characters long'); 
+    }
+    static password ( password ) {   
+        if ( typeof password !== 'string' ) throw new Error('Password must be a string');
+        if ( password.length < 6 ) throw new Error('Password must be at least 6 characters long'); 
+    }
+}
